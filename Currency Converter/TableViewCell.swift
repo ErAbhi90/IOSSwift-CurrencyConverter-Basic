2458eb2db1c65f6a8b45bc04f98463f6021aa0e7
ErAbhi90 <er.abhithakral@gmail.com>
2018-08-15 00:51:23 +0530

    Created first screen - Added custom tile rows

---
+++ Currency Converter/TableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewCell.swift
+//  Currency Converter
+//
+//  Created by Abhi on 14/08/18.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell1 : UITableViewCell{
+    
+}
+
+
+class TableViewCell2 : UITableViewCell{
+    
+}
